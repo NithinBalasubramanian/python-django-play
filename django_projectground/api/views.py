999af4ee6e82798821c75525762b98ae53800009
NithinBalasubrmanian <nithinfurie17@gmail.com>
2025-02-09 22:29:31 +0530

    feat: save and fetch data from numpy file and generate txt file

--- django_projectground/api/views.py
+++ django_projectground/api/views.py
@@ -15,7 +15,18 @@ def numpyRandomArray(request):
     })
 
 def numpyFileGeneration(request):
-    display_data = np.random.rand(2,2)*1000
-    return JsonResponse({
-        "data": display_data.astype(int).tolist()
-    })
+    try:
+        array1 = np.random.rand(2,2)*1000
+        array2 = np.random.rand(2,2)*1000
+        result = np.add(array1, array2)
+        np.save("./filesource/generated/numpy-files/random_array.npy", result)
+        result.astype(int).tofile("./filesource/generated/numpy-files/random_array.txt", sep=",")
+        fetchedResult = np.load("./filesource/generated/numpy-files/random_array.npy")
+        return JsonResponse({
+            "message": "array saved as npy and text file and fethed successfully",
+            "data": fetchedResult.astype(int).tolist()
+        })
+    except Exception as e:
+        return JsonResponse({
+            "error": str(e)
+        }, status=500)
